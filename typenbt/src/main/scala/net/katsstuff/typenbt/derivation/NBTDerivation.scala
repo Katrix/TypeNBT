95b0f02de9f17c9b5edaea566e37dd3a36a4b80d
Katrix <katrix97@hotmail.com>
2018-08-18 19:54:30 +0200

    Derivation mostly completed

--- typenbt/src/main/scala/net/katsstuff/typenbt/derivation/NBTDerivation.scala
+++ typenbt/src/main/scala/net/katsstuff/typenbt/derivation/NBTDerivation.scala
@@ -20,16 +20,38 @@ class NBTDerivation(val c: blackbox.Context) {
     typeOf[String]           -> typeOf[NBTString],
     //typeOf[Seq] -> typeOf[NBTList],
     typeOf[Map[String, _]]  -> compoundTpe,
-    typeOf[IndexedSeq[Int]] -> typeOf[NBTIntArray]
+    typeOf[IndexedSeq[Int]] -> typeOf[NBTIntArray],
+    typeOf[IndexedSeq[Long]] -> typeOf[NBTLongArray]
   )
 
   def createView[A: WeakTypeTag]: Expr[NBTView[A, NBTCompound]] = {
     val tpe = weakTypeOf[A]
 
+    if(!tpe.typeSymbol.isClass) {
+      c.abort(c.enclosingPosition, s"$tpe is not a class or trait")
+    }
+
     if (nbtTypeMap.values.exists(t => tpe =:= t)) {
       c.abort(c.enclosingPosition, s"$tpe is an NBT type, you can't create a new view for that")
     }
 
+    val classSymbol = tpe.typeSymbol.asClass
+
+    if(classSymbol.isCaseClass) {
+      deriveCaseClass(tpe)
+    }
+    else if(classSymbol.isTrait && classSymbol.isSealed) {
+      val subclasses = classSymbol.knownDirectSubclasses
+      if(subclasses.isEmpty) {
+        c.abort(c.enclosingPosition, s"No known subclasses for $tpe")
+      }
+
+      deriveSealedTrait(tpe, subclasses)
+    }
+    else c.abort(c.enclosingPosition, s"$tpe is not a case class or sealed trait")
+  }
+
+  def deriveCaseClass[A](tpe: Type): Expr[NBTView[A, NBTCompound]] = {
     val optPrimaryCtor = tpe.decls
       .collectFirst {
         case m: MethodSymbol if m.isPrimaryConstructor => m
@@ -49,19 +71,19 @@ class NBTDerivation(val c: blackbox.Context) {
     val fieldsToNamesAndCode = fields.map { field =>
       val fieldType        = field.typeSignatureIn(tpe)
       val fieldName        = field.name.toTermName.decodedName.toString
-      val serializerName   = TermName(c.freshName(fieldName + "serializer"))
-      val deserializerName = TermName(c.freshName(fieldName + "deserializer"))
-      val refinerName      = TermName(c.freshName(fieldName + "refiner"))
+      val serializerName   = TermName(c.freshName(fieldName + "Serializer"))
+      val deserializerName = TermName(c.freshName(fieldName + "Deserializer"))
+      val refinerName      = TermName(c.freshName(fieldName + "Refiner"))
       val code = Seq(
-        q"""val $serializerName = _root_.net.katsstuff.typenbt.NBTSerializer.forRepr[$fieldType].find""",
-        q"""val $deserializerName = _root_.net.katsstuff.typenbt.NBTDeserializer.forRepr[$fieldType].find""",
-        q"""val $refinerName = _root_.net.katsstuff.typenbt.NBTRefiner.fromDeserializer($deserializerName)"""
+        q"""private val $serializerName = _root_.net.katsstuff.typenbt.NBTSerializer.forRepr[$fieldType].find""",
+        q"""private val $deserializerName = _root_.net.katsstuff.typenbt.NBTDeserializer.forRepr[$fieldType].find""",
+        q"""private val $refinerName = _root_.net.katsstuff.typenbt.NBTRefiner.fromDeserializer($deserializerName)"""
       )
 
       (field, (code, serializerName, deserializerName, refinerName))
     }.toMap
 
-    val viewVals = fieldsToNamesAndCode.values.map(_._1).toList.flatten
+    val codeVals = fieldsToNamesAndCode.values.map(_._1).toList.flatten
 
     val (toCompound, fromCompound) = fieldsToNamesAndCode.map {
       case (field, (_, serializerName, deserializerName, refinerName)) =>
@@ -69,74 +91,83 @@ class NBTDerivation(val c: blackbox.Context) {
         val key  = name.decodedName.toString
 
         (
-          q"$key -> $serializerName.to(v.$name)",
+          q"($key, $serializerName.to(v.$name))",
           fq"$name <- arg.get($key).flatMap($refinerName.refine).flatMap($deserializerName.from)"
         )
     }.unzip
 
     val resTree = q"""
          new _root_.net.katsstuff.typenbt.NBTView[$tpe, _root_.net.katsstuff.typenbt.NBTCompound] {
-             ..$viewVals
+             ..$codeVals
 
            override def to(v: $tpe): _root_.net.katsstuff.typenbt.NBTCompound = {
-             _root_.net.katsstuff.typenbt.NBTCompound(Map(..$toCompound))
+             _root_.net.katsstuff.typenbt.NBTCompound(_root_.scala.Predef.Map(..$toCompound))
            }
 
-           override def from(arg: _root_.net.katsstuff.typenbt.NBTCompound): Option[$tpe] =
+           override def from(arg: _root_.net.katsstuff.typenbt.NBTCompound): _root_.scala.Option[$tpe] =
              for(..$fromCompound) yield new $tpe(..${fields.map(_.name.toTermName)})
          }
        """
 
     val res = c.Expr[NBTView[A, NBTCompound]](resTree)
 
-    c.echo(c.enclosingPosition, resTree.toString())
+    //c.info(c.enclosingPosition, showCode(resTree), force = false)
 
     res
   }
 
-  def createViewOld[A: WeakTypeTag]: Expr[NBTView[A, NBTCompound]] = {
-    val tpe       = weakTypeOf[A]
-    val companion = tpe.typeSymbol.companion
+  def deriveSealedTrait[A](tpe: Type, subClasses: Set[Symbol]): Expr[NBTView[A, NBTCompound]]  = {
+    val subClassesToNamesAndCode = subClasses.map { subClass =>
+      val fieldType        = subClass.asType
+      val fieldName        = subClass.name.toTermName.decodedName.toString
+      val serializerName   = TermName(c.freshName(fieldName + "Serializer"))
+      val deserializerName = TermName(c.freshName(fieldName + "Deserializer"))
+      val refinerName      = TermName(c.freshName(fieldName + "Refiner"))
 
-    val fields = tpe.decls
-      .collectFirst {
-        case m: MethodSymbol if m.isPrimaryConstructor => m
-      }
-      .get
-      .paramLists
-      .head
+      val code = Seq(
+        q"""private val $serializerName = _root_.net.katsstuff.typenbt.NBTSerializer.forRepr[$fieldType].find""",
+        q"""private val $deserializerName = _root_.net.katsstuff.typenbt.NBTDeserializer.forRepr[$fieldType].find""",
+        q"""private val $refinerName = _root_.net.katsstuff.typenbt.NBTRefiner.fromDeserializer($deserializerName)"""
+      )
 
-    val fieldsToNbt = fields.map { field =>
-      val fieldType = field.typeSignature
-      val optNbt    = nbtTypeMap.get(fieldType)
-      field -> optNbt.getOrElse(typeOf[NBTCompound])
+      (subClass, (code, serializerName, deserializerName, refinerName))
     }.toMap
 
-    val (toCompound, fromCompound) = fields.map { field =>
-      val nbt           = fieldsToNbt(field)
-      val name          = field.asTerm.name
-      val typeSignature = field.typeSignature
-      val key           = name.decodedName.toString
+    val codeVals = subClassesToNamesAndCode.values.map(_._1).toList.flatten
 
-      (
-        q"$key -> io.github.katrix.typenbt.nbt.NBTView[$typeSignature, $nbt].apply(v.$name)",
-        q"nbt.getValue[$nbt, $typeSignature]($key)"
-      )
+    val (toCompound, fromCompound) = subClassesToNamesAndCode.map {
+      case (subClass, (_, serializerName, deserializerName, refinerName)) =>
+        val subClassTpe = subClass.asType
+        val typeName  = subClass.name.toTermName.decodedName.toString
+
+        (
+          cq"""value: $subClassTpe => _root_.scala.Predef.Map(("$$type", _root_.net.katsstuff.typenbt.NBTString($typeName)), ("$$value", $serializerName.to(value)))""",
+          cq"""_root_.net.katsstuff.typenbt.NBTString($typeName) => arg.get("$$value").flatMap($refinerName.refine).flatMap($deserializerName.from)"""
+        )
     }.unzip
 
-    val res = c.Expr[NBTView[A, NBTCompound]] {
-      q"""
-        new io.github.katrix.typenbt.nbt.NBTView[$tpe, io.github.katrix.typenbt.nbt.NBTCompound] {
-          override implicit def apply(v: $tpe): io.github.katrix.typenbt.nbt.NBTCompound = io.github.katrix.typenbt.nbt.NBTCompound(Map(..$toCompound))
-          override implicit def unapply(nbt: io.github.katrix.typenbt.nbt.NBTCompound): Option[$tpe] = {
-            val valid = !Seq(..$fromCompound).contains(None)
-            if(valid) Some($companion(..${fromCompound.map(t => q"$t.get")})) else None
-          }
-        }
-     """
-    }
+    val fromPartial = q"{ case ..$fromCompound }"
 
-    //c.info(c.enclosingPosition, res.toString(), force = true)
+    val resTree = q"""
+         new _root_.net.katsstuff.typenbt.NBTView[$tpe, _root_.net.katsstuff.typenbt.NBTCompound] {
+             ..$codeVals
+
+           override def to(v: $tpe): _root_.net.katsstuff.typenbt.NBTCompound = {
+             val nbtMap = v match {
+               case ..$toCompound
+             }
+             _root_.net.katsstuff.typenbt.NBTCompound(nbtMap)
+           }
+
+           override def from(arg: _root_.net.katsstuff.typenbt.NBTCompound): Option[$tpe] = arg.get("$$type").collect {
+             case ..$fromCompound
+           }.flatten
+         }
+       """
+
+    val res = c.Expr[NBTView[A, NBTCompound]](resTree)
+
+    //c.info(c.enclosingPosition, showCode(resTree), force = false)
 
     res
   }
--- typenbtExtra/src/main/scala/net/katsstuff/typenbt/extra/DerivTest.scala
+++
@@ -1,8 +0,0 @@
-package net.katsstuff.typenbt.extra
-import net.katsstuff.typenbt.{NBTCompound, NBTView}
-
-object DerivTest {
-  case class Foo(i: Int, str: String, bar: Double)
-  val fooView: NBTView[Foo, NBTCompound] = net.katsstuff.typenbt.derivation.semiauto.deriveView[Foo]
-
-}
